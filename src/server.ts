7972416efa6144ef7526e160282a6bc195c847d6
Williams Calado <williamscalado@gmail.com>
2022-01-22 14:02:09 -0300

    Adding concept the MVC!

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 import mainroutes from './routes/index';
 import loginroutes from './routes/login'
 
 const server = express()
+
+// difinindo rotas de arquivos estaticos e publico
+let url = path.join(__dirname, '../public')
+server.use(express.static(url))
+
+//permite o uso do corpo da pagina para obter por exemplo dados VIA POST
+server.use(express.urlencoded({ extended: true }))
+
 // formas de usar as rotas 
 server.use('/', mainroutes)
 server.use('/login', loginroutes)
@@ -11,6 +20,7 @@ server.use('/login', loginroutes)
 server.use((req: Request, res: Response) => {
     res.status(404).send('Ops! page not found!')
 })
+
 server.listen(3000)
 
 
